dc64b37c7f9f5f87729ab0709f75584fccfbc88b
Gabriel Andrade <andrade.gabriel2295@gmail.com>
2023-01-20 06:22:17 -0300

    initial commit

---
+++ src/main/java/med/voll/api/controller/PacientesController.java
@@ -0,0 +1,13 @@
+package med.voll.api.controller;
+
+import org.springframework.web.bind.annotation.RequestBody;
+
+import med.voll.api.paciente.DadosCadastroPaciente;
+
+public class PacientesController {
+
+    public void cadastrar(@RequestBody DadosCadastroPaciente dados) {
+        
+    }
+    
+}
